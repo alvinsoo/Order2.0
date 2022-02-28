00b24e6df933ff9c6a6d4b400b6530a4e22c3d01
alvinsoo <34090577+alvinsoo@users.noreply.github.com>
2022-02-28 17:00:03 +0800

    Add files via upload

---
+++ Order2.0/Controllers/OrderProcessor.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace payments_api.Consumer
+{
+    public class OrderProcessor : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public OrderProcessor(ILoggerFactory loggerFactory)
+        {
+            this._logger = loggerFactory.CreateLogger<OrderProcessor>();
+            InitRabbitMQ();
+        }
+
+        private void InitRabbitMQ()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 31672
+            };
+
+            // create connection  
+            _connection = factory.CreateConnection();
+
+            // create channel  
+            _channel = _connection.CreateModel();
+
+            //_channel.ExchangeDeclare("demo.exchange", ExchangeType.Topic);
+            _channel.QueueDeclare("orders", false, false, false, null);
+            // _channel.QueueBind("demo.queue.log", "demo.exchange", "demo.queue.*", null);
+            // _channel.BasicQos(0, 1, false);
+
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                // received message  
+                var content = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                // handle the received message  
+                HandleMessageAsync(content);
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+            _channel.BasicConsume("orders", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessageAsync(string content)
+        {
+            // we just print this message   
+            _logger.LogInformation($"consumer received {content}");
+
+
+
+            string[] tokens = content.Split(":");
+            string[] ordersStatus = tokens[3].Split("|");
+            string[] orderId = tokens[2].Split(",");
+            string[] cartId = tokens[1].Split(",");
+
+
+            _logger.LogInformation($"after received {ordersStatus[0]}");
+            _logger.LogInformation($"Processing order... {ordersStatus[0]} ");
+
+            await Task.Delay(30000);
+
+            _logger.LogInformation($"will be published with updated OrderStatus {ordersStatus[0]}");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 31672
+            };
+
+            Console.WriteLine(factory.HostName + ":" + factory.Port);
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "orderstatus",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "OrderId=" + orderId[0] + ", CartId=" + cartId[0] + ", ordersStatus=Success";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "orderstatus",
+                                     basicProperties: null,
+                                     body: body);
+            }
+
+        }
+
+        private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e) { }
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e) { }
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e) { }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+    }
+}
